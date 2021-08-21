0776ab9ba5e23396168b1677c67e17df69641a28
Jared Grimes <jaredlgrimes@hotmail.com>
2021-08-20 21:08:25 -0400

    updated how long

--- commands/cute/how-long.js
+++ commands/cute/how-long.js
@@ -2,6 +2,12 @@ const { Command } = require('discord.js-commando');
 const fetch = require('node-fetch');
 const moment = require('moment');
 
+const DAYS_WE_MEET = [
+  '2021-09-24T13:44:00-04:00',
+  '2021-10-22T13:44:00-04:00',
+  '2021-12-25T13:44:00-04:00'
+];
+
 module.exports = class SetCommand extends Command {
   constructor(client) {
     super(client, {
@@ -16,7 +22,7 @@ module.exports = class SetCommand extends Command {
 
   async run(message) {
     const todaysDate = moment();
-    const dateWeMeet = moment('2021-05-13T13:44:00-04:00')
+    const dateWeMeet = moment.min(DAYS_WE_MEET.map(d => moment(d)));
     
     message.channel.send(`You will be in my arms again ${todaysDate.to(dateWeMeet)} <3`);
   }
